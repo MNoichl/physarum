89fb1c8845ea9a0af1432fc907a5eb307cd7cc5e
MNoichl <noichlmax@hotmail.co.uk>
2020-12-08 03:16:13 +0100

    physarum first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='physarum',
+      version='0.1',
+      description='Simulate physarum interacting populations.',
+      url='https://github.com/MNoichl/physarum',
+      author='Maximilian Noichl',
+      author_email='noichlmax@hotmail.co.uk',
+      license='MIT',
+      packages=['physarum'],
+      zip_safe=False)
